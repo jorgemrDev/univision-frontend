11c5e604cb4c714e589c52b806fad3c50f902589
Jorge Moreno <black.spade.666@gmail.com>
2021-05-12 00:00:42 -0500

    initial  commit

---
+++ univision-videos-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+//https://stackoverflow.com/questions/63748312/react-bootstrap-cards-and-carddeck-components-responsive-layout
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
