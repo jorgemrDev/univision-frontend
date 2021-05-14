e0d8f03927db9d7ffed13a2dea5c2e43b7c129df
jorgemrDev <black.spade.666@gmail.com>
2021-05-14 16:47:23 -0500

    Update constants.js

--- univision-videos-frontend/src/utils/constants.js
+++ univision-videos-frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 //const API_URL = "http://localhost:26765/";
-//const API_URL = "https://app-dev-003.azurewebsites.net/";
+const API_URL = "https://app-dev-003.azurewebsites.net/";
 const STATIC_FOLDER_FROM_SERVER = "StaticFiles";
 
 export { API_URL, STATIC_FOLDER_FROM_SERVER };
